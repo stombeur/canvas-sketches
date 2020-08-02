b0cfa13f255963f0f124ba72ce3f26d3908a58fb
Stephane Tombeur <stephane@tombeur.be>
2020-08-02 19:01:00 +0200

    folders

---
+++ circles/circle-hatches-skewed-large.js
@@ -0,0 +1,179 @@
+// hatched/skewed circles
+// multi-page side-by-side
+
+const canvasSketch = require('canvas-sketch');
+const penplot = require('../utils/penplot');
+const utils = require('../utils/random');
+const poly = require('../utils/poly');
+
+let svgFile = new penplot.SvgFile();
+let svgFile2 = new penplot.SvgFile();
+let svgFile3 = new penplot.SvgFile();
+
+const settings = {
+  dimensions: [(29.7 * 3), 42.0],
+  orientation: 'landscape',
+  pixelsPerInch: 300,
+  scaleToView: true,
+  units: 'cm',
+};
+
+const sketch = (context) => {
+
+  let margin = 0.2;
+  let elementWidth = 1;
+  let elementHeight = 1;
+  let columns = 1;
+  let rows = 1;
+  
+  let drawingWidth = (columns * (elementWidth + margin)) - margin;
+  let drawingHeight = (rows * (elementHeight + margin)) - margin;
+  let marginLeft = (context.width - drawingWidth) / 2;
+  let marginTop = (context.height - drawingHeight) / 2;
+  
+  // let o = [];
+  // for (let r = 0; r < rows; r++) {
+  //   o[r] = [];
+  //   for (let i = 0; i < columns; i++) {
+  //     let rot = utils.getRandomInt(4,0) * 90;//utils.random(0, 360);
+  //     let size = utils.random(45, 270);
+  //     let quarter = utils.getRandomInt(4,1);
+  //     o[r].push([rot, size, quarter]);
+  //   }
+  // }
+  
+  return ({ context, twidth, theight, units }) => {
+
+    svgFile = new penplot.SvgFile();
+    svgFile2 = new penplot.SvgFile();
+    svgFile3 = new penplot.SvgFile();
+    poly.init(context);
+
+    let width = 29.7,
+        height = 42.0;
+
+    const drawBounds = (w, h, m, offsetX, options = {}) => {
+      let result = [];
+
+      let x = offsetX + m;
+      let y = m;
+
+      result.push([x, y]);
+      result.push([x + w - 2*m, y]);
+      result.push([x + w - 2*m, y + h - 2*m]);
+      result.push([x, y + h - 2*m]);
+
+      context.beginPath();
+  
+      context.strokeStyle = options.strokeStyle || 'black';
+      context.lineWidth = options.lineWidth || 0.01;
+      context.lineCap = options.lineCap || 'square';
+      context.lineJoin = options.lineJoin || 'miter';
+    
+      context.moveTo(result[0][0], result[0][1]);
+    
+      result.map(function(point) {
+        context.lineTo(point[0], point[1]);
+      });
+    
+      context.lineTo(result[0][0], result[0][1]);
+    
+      context.stroke();
+    }
+    
+
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    context.strokeStyle = 'black';
+    context.lineWidth = 0.01;
+
+    drawBounds(width, height, margin, 0);
+    drawBounds(width, height, margin, width);
+    drawBounds(width, height, margin, width * 2);
+
+    let bb = {xmin:margin, ymin:margin, xmax:width-margin, ymax:height-margin};
+    let bb2 = {xmin:width + margin, ymin:margin, xmax:(2*width)-margin, ymax:height-margin};
+    let bb3 = {xmin:(2*width) + margin, ymin:margin, xmax:(3*width)-margin, ymax:height-margin};
+
+
+    let circles = [];
+    circles.push({center: poly.point(width/3, height/3), r: width/2, hatchAngle: 20, hatchSpace: 0.17, innerOuter: 4/5, innerCenter: poly.point((width/3)+1.5, (height/3)+1.5)});
+    //circles.push({center: poly.point(width/4, height/4), r: width/4, hatchAngle: 25, hatchSpace: 0.1, innerOuter: 0.5, innerCenter: poly.point((width/4)+0.8, (height/4)+1.2)});
+    circles.push({center: poly.point(width/3*2, height/3*2), r: width/2, hatchAngle: 70, hatchSpace: 0.16, innerOuter: 3.5/5, innerCenter: poly.point((width/3*2)-1, (height/3*2)+0.8)});
+    circles.push({center: poly.point(width-5, height/4*3), r: width/2+2, hatchAngle: 44, hatchSpace: 0.18, innerOuter: 3.5/5, innerCenter: poly.point(width-4, height/4*3+1)});
+
+    circles.push({center: poly.point(-width/2, height/2+4), r: width, hatchAngle: 85, hatchSpace: 0.17, innerOuter: 0.77, innerCenter: poly.point((-width/2)+1.5, (height/2+4)-0.2)});
+
+    circles.push({center: poly.point(width+2, height/3), r: width/2, hatchAngle: 20, hatchSpace: 0.15, innerOuter: 4/5, innerCenter: poly.point((width)+3.5, (height/3)+1.5)});
+    circles.push({center: poly.point(width*2-8, width+5), r: width/2+2, hatchAngle: 41, hatchSpace: 0.14, innerOuter: 4.1/5, innerCenter:poly.point(width*2-10, width+6)});
+
+    circles.push({center: poly.point(width*2+3, height/2+2), r: width/2+2, hatchAngle: 33, hatchSpace: 0.14, innerOuter: 4.1/5, innerCenter:poly.point(width*2+4, height/2+1)});
+    circles.push({center: poly.point(width*3, height/2+2), r: height, hatchAngle: 12, hatchSpace: 0.14, innerOuter: 4.3/5, innerCenter:poly.point(width*3, height/2+6)});
+    circles.push({center: poly.point(width*2.5-3, height*1.2), r: width/2+4, hatchAngle: 7, hatchSpace: 0.14, innerOuter: 4.1/5, innerCenter:poly.point(width*2.5-1.5, height*1.2-2)});
+
+
+    circles.forEach(c => {
+
+      let x = poly.hatchDonut(c.center, c.r, c.r * c.innerOuter, c.hatchAngle, c.hatchSpace, c.innerCenter);
+      x.forEach(l => { 
+        let clippedLine = poly.clipLineToBB(l, bb);
+        if (clippedLine) { 
+          poly.drawLineOnCanvas(clippedLine);
+          let p1 = [clippedLine[0][0] || clippedLine[0].x, clippedLine[0][1] || clippedLine[0].y];
+          let p2 = [clippedLine[1][0] || clippedLine[1].x, clippedLine[1][1] || clippedLine[1].y];
+          svgFile.addLine([p1, p2]);
+         }
+
+         let clippedLine2 = poly.clipLineToBB(l, bb2);
+        if (clippedLine2) { 
+          poly.drawLineOnCanvas( clippedLine2);
+          let p1 = [clippedLine2[0][0] - width || clippedLine2[0].x - width, clippedLine2[0][1] || clippedLine2[0].y];
+          let p2 = [clippedLine2[1][0] - width || clippedLine2[1].x - width, clippedLine2[1][1] || clippedLine2[1].y];
+          svgFile2.addLine([p1, p2]);
+         }
+
+          clippedLine = poly.clipLineToBB(l, bb3);
+        if (clippedLine) { 
+          poly.drawLineOnCanvas( clippedLine);
+          let p1 = [clippedLine[0][0] - width*2 || clippedLine[0].x - width*2, clippedLine[0][1] || clippedLine[0].y];
+          let p2 = [clippedLine[1][0] - width*2 || clippedLine[1].x - width*2, clippedLine[1][1] || clippedLine[1].y];
+          svgFile3.addLine([p1, p2]);
+         }
+      });
+
+      svgFile.newPath();
+    });
+
+    return [
+      // Export PNG as first layer
+      context.canvas,
+      // Export SVG for pen plotter as second layer
+      {
+        data: svgFile.toSvg({
+          width,
+          height,
+          units
+        }),
+        extension: '.svg',
+      },
+      {
+        data: svgFile2.toSvg({
+          width,
+          height,
+          units
+        }),
+        extension: '.svg',
+      },
+      {
+        data: svgFile3.toSvg({
+          width,
+          height,
+          units
+        }),
+        extension: '.svg',
+      }
+    ];
+  };
+};
+
+canvasSketch(sketch, settings);
---
+++ hatches/hatched-hexagons-big.js
@@ -0,0 +1,210 @@
+const canvasSketch = require('canvas-sketch');
+const { polylinesToSVG } = require('canvas-sketch-util/penplot');
+const utils = require('../utils/random');
+const poly = require('../utils/poly');
+const polygonBoolean = require('2d-polygon-boolean');
+
+
+const lines = [];
+
+const settings = {
+  dimensions: 'A3',
+  orientation: 'portrait',
+  pixelsPerInch: 300,
+  scaleToView: true,
+  units: 'cm',
+};
+
+// const rotate = (point, center, angle) => {
+//   if (angle === 0) return point;
+
+//   let radians = (Math.PI / 180) * angle,
+//     x = point[0],
+//     y = point[1],
+//     cx = center[0],
+//     cy = center[1],
+//     cos = Math.cos(radians),
+//     sin = Math.sin(radians),
+//     nx = cos * (x - cx) - sin * (y - cy) + cx,
+//     ny = cos * (y - cy) + sin * (x - cx) + cy;
+//   return [nx, ny];
+// };
+
+const rotatePoly = (polyLine, center, angle) => {
+  return polyLine.map(p => {
+    return poly.rotatePoint(p, center, angle);
+  });
+};
+
+// const createPolygon = (nrOfSides, sideLength, centerPoint) => {
+//   let centerAngle = Math.PI * 2 / nrOfSides;
+//   let b = Math.sin(centerAngle/2) * sideLength / 2 / Math.cos(centerAngle/2);
+//   let x1 = centerPoint[0] - sideLength / 2;
+//   let y1 = centerPoint[1] + b;
+
+//   let polygon = [];
+//   polygon.push([x1, y1]);
+//   for (let i = 1; i < nrOfSides; i++) {
+//     polygon.push(poly.rotatePoint([x1, y1], centerPoint, 360 / nrOfSides * i))
+//   }
+ 
+//   return polygon;
+// }
+
+// function drawPolygon(context, poly) {
+//   context.beginPath();
+
+//   context.strokeStyle = 'black';
+//   context.lineWidth = 0.01;
+//   context.lineCap = 'square';
+//   context.lineJoin = 'miter';
+
+//   context.moveTo(poly[0][0], poly[0][1]);
+
+//   poly.map(function(point) {
+//     context.lineTo(point[0], point[1]);
+//   });
+
+//   context.lineTo(poly[0][0], poly[0][1]);
+
+//   context.stroke();
+
+//   lines.push(poly);
+// }
+
+function drawLine(line) {
+  poly.drawLineOnCanvas(line);
+  lines.push(line);
+}
+
+// const boundingBox = (polyLine, padding = 0) => {
+//   let left = Number.MAX_VALUE,
+//     top = Number.MAX_VALUE,
+//     right = 0,
+//     bottom = 0;
+
+//   polyLine.map(p => {
+//     left = Math.min(p[0], left);
+//     top = Math.min(p[1], top);
+//     right = Math.max(p[0], right);
+//     bottom = Math.max(p[1], bottom);
+//   });
+
+//   return [
+//     [left - padding, top - padding],
+//     [right + padding, top - padding],
+//     [right + padding, bottom + padding],
+//     [left - padding, bottom + padding]
+//   ];
+// };
+
+const hatch2 = (polygon, angle, spacing = 0.1) => {
+  let rectangle = poly.calculateBoundingBox(polygon);//boundingBox(polygon);
+  //console.log(rectangle);
+  let x = rectangle[0][0];
+  let y = rectangle[0][1];
+  let height = rectangle[2][1] - y;
+  let width = rectangle[2][0] - x;
+  //console.log({x,y,height, width});
+  let rotatedRectangle = poly.rotatePolygon(
+    rectangle,
+    [x + width / 2, y + width / 2],
+    angle
+  );
+  //console.log(rotatedRectangle);
+  let box = poly.calculateBoundingBox(rotatedRectangle, 0);
+
+  let x2 = box[0][0];
+  let y2 = box[0][1];
+  let height2 = box[2][1] - y2;
+  let width2 = box[2][0] - x2;
+
+  let numLines = Math.floor(height2 / spacing);
+  let result = [];
+
+  for (let i = 0; i <= numLines; i++) {
+    let line = [[x2, y2 + i * spacing], [x2 + width2, y2 + i * spacing]];
+    let rotatedLine = poly.rotatePolygon(line, [x + width / 2, y + width / 2], angle);
+    result.push(rotatedLine);
+  }
+  return result;
+};
+
+const clip = (line, polyClip) => {
+  let closedLine = [line[0], line[1], line[1], line[0]];
+  //console.log(closedLine);
+  return polygonBoolean(polyClip, closedLine, 'and');
+};
+
+const drawHatchedPoly = (posX, posY, angle, space, sideLength = 2) => {
+  let rect = poly.createPolygon(6, sideLength, [posX, posY]);
+  let hatched = poly.hatchPolygon(rect, angle, space);
+  //console.dir(hatched);
+  //hatched = hatch2(rect, angle, space);
+  //console.dir(hatched);
+  for (let i = 0; i < hatched.length; i++) {
+    let x = null;
+    try {
+      x = clip(hatched[i], rect);
+    } catch {}
+    if (x) {
+      x.map(l => {
+        drawLine([l[0], l[1]]);
+      });
+    }
+  }
+};
+
+const sketch = (context) => {
+
+  let margin = 0.2;
+  let elementWidth = 2;
+  let elementHeight = 1.8;
+  let columns = 6;
+  let rows = 12;
+  
+  let drawingWidth = (columns * (elementWidth + margin)) - margin;
+  let drawingHeight = (rows * (elementHeight + margin)) - margin;
+  let marginLeft = (context.width - drawingWidth) / 2;
+  let marginTop = (context.height - drawingHeight) / 2;
+  
+  let o = [];
+  for (let r = 0; r < rows; r++) {
+    o[r] = [];
+    for (let i = 0; i < columns; i++) {
+      let rot = utils.random(-89, 89);
+      let space = utils.random(0.1,0.30);
+      o[r].push([rot,space]);
+    }
+  }
+
+  return ({ context, width, height, units }) => {
+    poly(context);
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    let posX = marginLeft + elementWidth / 4;
+    let posY = marginTop  + elementHeight / 4;
+
+    //poly.drawHatchedPolygonOnCanvas(context, 13,17, 12.3, 0.45, 14);
+    drawHatchedPoly(13,17, 12.3, 0.45, 14);
+    drawHatchedPoly(16,25, 54.8, 0.85, 14);
+
+    return [
+      // Export PNG as first layer
+      context.canvas,
+      // Export SVG for pen plotter as second layer
+      {
+        data: polylinesToSVG(lines, {
+          width,
+          height,
+          units
+        }),
+        extension: '.svg',
+        //file: 'c:\\tmp\\saveme.svg'
+      }
+    ];
+  };
+};
+
+canvasSketch(sketch, settings);
