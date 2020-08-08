656c2c1488bf199e216531dbc2ec75305f017387
Stephane Tombeur <stephane@tombeur.be>
2020-08-08 20:32:07 +0200

    crosshairs

--- circle-crosshairs-hatches.js
+++ circle-crosshairs-hatches.js
@@ -1,21 +1,26 @@
 // hatched donuts
 
 const canvasSketch = require('canvas-sketch');
+const { renderGroups, renderPaths, createPath } = require('canvas-sketch-util/penplot');
+
 const penplot = require('./utils/penplot');
 const poly = require('./utils/poly');
 const polybool = require('polybooljs');
 const random = require('canvas-sketch-util/random');
 let svgFile = new penplot.SvgFile();
 
+random.setSeed(random.getRandomSeed());//359079
+
 const settings = {
-  dimensions: 'A3',
+  dimensions: 'A4',
   orientation: 'portrait',
   pixelsPerInch: 300,
   scaleToView: true,
   units: 'cm',
 };
 
-const sketch = (context) => {
+const sketch = ({ width, height }) => {
+  paths = [];
 
   let margin = 1;
   let elementWidth = 1;
@@ -25,22 +30,34 @@ const sketch = (context) => {
   
   let drawingWidth = (columns * (elementWidth + margin)) - margin;
   let drawingHeight = (rows * (elementHeight + margin)) - margin;
-  let marginLeft = (context.width - drawingWidth) / 2;
-  let marginTop = (context.height - drawingHeight) / 2;
+  let marginLeft = (width - drawingWidth) / 2;
+  let marginTop = (height - drawingHeight) / 2;
    
   return ({ context, width, height, units }) => {
-    svgFile = new penplot.SvgFile();
+    paths = [];
 
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    context.strokeStyle = 'black';
-    context.lineWidth = 0.01;
-    poly.init(context);
 
+    const drawLineOnCanvas = (ctx, line) => {
+      let x1 = line[0].x  || line[0][0],
+          x2 = line[1].x || line[1][0],
+          y1 = line[0].y || line[0][1],
+          y2 = line[1].y || line[1][1];
+
+      //console.log({line:[[x1,y1],[x2,y2]]})
     
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+    }
 
-    const drawSegment = (rOuter, rInner, center, sAngle, eAngle, hAngle) => {
+    const drawSegment = (rOuter, rInner, center, sAngle, eAngle) => {
+      if (random.chance(0.25)) { return; }
+
       let rot = eAngle-sAngle;
+
+      let hAngle = random.pick([15,30,45,60]);
+      let spacing = random.pick([0.1,0.2,0.3,0.7,0.5,0.6]);
 
       let p1 = poly.point(center.x, center.y - rOuter);
       if (sAngle>0) p1 = poly.rotatePointXY(p1, center, sAngle);
@@ -63,11 +80,15 @@ const sketch = (context) => {
       let polygon = [p4,p1].concat(interPointsMax).concat([p2,p3]).concat(interPointsMin);
       //poly.drawPolygon(context)(polygon);
   
-      let hatches = poly.hatchPolygon(polygon, hAngle, 0.5);
+      let hatches = poly.hatchPolygon(polygon, hAngle, spacing, 2);
   
-      hatches.forEach(h => {
-        poly.drawLineOnCanvas(h)
+      const path = createPath(ctx => {
+        hatches.forEach(h => {
+          drawLineOnCanvas(ctx, h)
+        });
       });
+
+      paths.push(path);
     }
 
     let rmax = (width-margin*2) / 2;
@@ -76,6 +97,8 @@ const sketch = (context) => {
 
     let nrOfBands = 8;
     let nrOfSegments = 10;
+    let spacebetweensegments = 0.1;
+    let anglebetweensegments = 1;
 
     for (let i = 0; i < nrOfSegments; i++) {
       let sAngle = 360 / nrOfSegments * i;
@@ -83,9 +106,8 @@ const sketch = (context) => {
       
       for (let j = 1; j < nrOfBands; j++) {
         let rout = rmax / nrOfBands * (j+1);
-        let rin = rmax / nrOfBands * j;
-        let hAngle = random.pick([15,30,45,60]);
-        drawSegment(rout, rin, center, sAngle, eAngle, hAngle);
+        let rin = rmax / nrOfBands * j + spacebetweensegments;
+        drawSegment(rout, rin, center, sAngle + (anglebetweensegments/2), eAngle - (anglebetweensegments/2));
       }
     }
     
@@ -100,19 +122,9 @@ const sketch = (context) => {
         
         
 
-    return [
-      // Export PNG as first layer
-      context.canvas,
-      // Export SVG for pen plotter as second layer
-      {
-        data: svgFile.toSvg({
-          width,
-          height,
-          units
-        }),
-        extension: '.svg',
-      }
-    ];
+    return renderGroups([paths], {
+      context, width, height, units
+    });
   };
 };
 
