3666c106c497d209206720bb14a8b19f5d9c932e
Stephane Tombeur <stephane@Ratchet.local>
2020-09-04 09:11:51 +0200

    circles-lines

---
+++ genart-010-circles-lines.js
@@ -0,0 +1,93 @@
+
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const { renderPaths, createPath } = require('canvas-sketch-util/penplot');
+const postcards = require('./utils/postcards');
+const random = require('canvas-sketch-util/random');
+
+let paths = [];
+
+const createGrid = (count, width, height) => {
+  const points = [];
+
+  const countX = count;
+  const countY = Math.floor(countX / width * height);
+
+  for (let x = 0; x < countX; x++) {
+    for (let y = 0; y < countY; y++) {
+      const u = x / (countX - 1);
+      const v = y / (countY - 1);
+      const position = [ u, v ];
+      const noise = random.noise2D(u,v);
+      const radius = Math.abs(noise) * 0.030;
+
+      points.push({ radius: Math.abs(radius),
+                    position,
+                    rotation: random.noise2D(u+2,v)
+                  });
+    }
+  }
+  return points;
+};
+
+const drawArcOnCanvas = (ctx, cx, cy, radius, sAngle, eAngle) => {
+  //ctx.beginPath();
+  ctx.arc(
+    cx,
+    cy,
+    radius,
+    (Math.PI / 180) * sAngle,
+    (Math.PI / 180) * eAngle
+  );
+  //ctx.stroke();
+};
+
+const settings = {
+  dimensions: 'A4',//[ 2048, 2048 ]
+  orientation: 'portrait',
+  pixelsPerInch: 300,
+  //scaleToView: true,
+  units: 'mm',
+};
+
+const sketch = ({ width, height }) => {
+  return ({ context, width, height, units }) => {
+    const count = 45;
+    paths = [];
+
+
+    const draw = (origin, w, h) => {
+      const margin = w * 0.20;
+      
+      random.setSeed(random.getRandomSeed());
+
+      let points = createGrid(count, w, h).filter(() => {
+        return Math.random() > 0.9975;
+      });
+
+      points.forEach(data => {
+        const {
+          position,
+          radius,
+          rotation
+        } = data;
+
+        const x0 = lerp(margin, w - margin, position[0]);
+        const y0 = lerp(margin, h - margin, position[1]);
+        const [x,y] = postcards.reorigin([x0,y0], origin);
+  
+        paths.push(createPath(ctx => {
+          drawArcOnCanvas(ctx, x, y, 2, 0, 360);
+        }));
+      });
+    }
+
+    postcards.drawQuad(draw, width, height);
+
+    return renderPaths([paths], {
+      context, width, height, units
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
