26667eabb4473a17a9aaf9873713b71c06869d02
Stephane Tombeur <stephane@Bluestreak.local>
2020-09-22 09:46:32 +0200

    postcards with index and options

--- utils/postcards.js
+++ utils/postcards.js
@@ -14,42 +14,64 @@ const reorigin = (point, origin) => {
     else return t;
   }
 
-  const drawQuad = (f, width, height) => {
-    f([0,0], width/2, height/2);
-    f([0,height/2], width/2, height/2);
-    f([width/2,0], width/2, height/2);
-    f([width/2,height/2], width/2, height/2);
+  const drawQuad = (f, width, height, opt = null) => {
+    let opt2 = {index:0}
+
+    opt2.index = 1;
+    f([0,0], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 2;
+    f([0,height/2], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 3;
+    f([width/2,0], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 4;
+    f([width/2,height/2], width/2, height/2, {...opt, ...opt2});
   }
-  const drawQuadQuad = (f, width, height) => {
-    drawOffsetQuad(f, width/2, height/2, [0,0]);
-    drawOffsetQuad(f, width/2, height/2, [width/2,0]);
-    drawOffsetQuad(f, width/2, height/2, [width/2,height/2]);
-    drawOffsetQuad(f, width/2, height/2, [0,height/2]);
+  
+  const drawQuadQuad = (f, width, height, opt = null) => {
+    drawOffsetQuad(f, width/2, height/2, [0,0], opt);
+    drawOffsetQuad(f, width/2, height/2, [width/2,0], opt);
+    drawOffsetQuad(f, width/2, height/2, [width/2,height/2], opt);
+    drawOffsetQuad(f, width/2, height/2, [0,height/2], opt);
   }
 
-  const drawOffsetQuad = (f, width, height, offsetOrigin) => {
+  const drawOffsetQuad = (f, width, height, offsetOrigin, opt = null) => {
     let [x,y] = [offsetOrigin[0] , offsetOrigin[1]];
+    let opt2 = {index:0}
 
-    f([x,y], width/2, height/2);
-    f([x,y+height/2], width/2, height/2);
-    f([x+width/2,y], width/2, height/2);
-    f([x+width/2,y+height/2], width/2, height/2);
+    opt2.index = 1;
+    f([x,y], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 2;
+    f([x,y+height/2], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 3;
+    f([x+width/2,y], width/2, height/2, {...opt, ...opt2});
+    opt2.index = 4;
+    f([x+width/2,y+height/2], width/2, height/2, {...opt, ...opt2});
   }
 
-  const drawSingle = (f, width, height) => {
-    f([0,0], width, height);
+  const drawSingle = (f, width, height, opt = null) => {
+    f([0,0], width, height, opt);
   }
 
-  const drawOct = (f, width, height) => {
-    f([0,0], width/4, height/2);
-    f([width/4,0], width/4, height/2);
-    f([width/2,0], width/4, height/2);
-    f([width/4*3,0], width/4, height/2);
+  const drawOct = (f, width, height, opt = null) => {
+    let opt2 = {index:0}
 
-    f([0,height/2], width/4, height/2);
-    f([width/4,height/2], width/4, height/2);
-    f([width/2,height/2], width/4, height/2);
-    f([width/4*3,height/2], width/4, height/2);
+    opt2.index = 1;
+    f([0,0], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 2;
+    f([width/4,0], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 3;
+    f([width/2,0], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 4;
+    f([width/4*3,0], width/4, height/2, {...opt, ...opt2});
+
+    opt2.index = 5;
+    f([0,height/2], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 6;
+    f([width/4,height/2], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 7;
+    f([width/2,height/2], width/4, height/2, {...opt, ...opt2});
+    opt2.index = 8;
+    f([width/4*3,height/2], width/4, height/2, {...opt, ...opt2});
 
   }
 
